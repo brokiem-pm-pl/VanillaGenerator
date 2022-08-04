64035352a1104d0fb530c5d8a2f0d9a0139aaab9
VixikHD <dev@vixikhd.me>
2022-08-05 00:23:35 +0200

    Import const

--- src/muqsit/vanillagenerator/generator/overworld/populator/biome/BiomePopulator.php
+++ src/muqsit/vanillagenerator/generator/overworld/populator/biome/BiomePopulator.php
@@ -25,6 +25,7 @@ use pocketmine\block\VanillaBlocks;
 use pocketmine\utils\Random;
 use pocketmine\world\ChunkManager;
 use pocketmine\world\format\Chunk;
+use const PHP_INT_MIN;
 
 class BiomePopulator implements Populator{
 

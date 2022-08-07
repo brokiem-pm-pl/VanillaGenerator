e88800cbe01ff131787f609f5a867055a5fcf17b
VixikHD <dev@vixikhd.me>
2022-08-08 00:22:34 +0200

    Fix & optimize Big oak tree generation
    - Big oak tree looked weird before this commit

--- src/muqsit/vanillagenerator/generator/object/tree/BigOakTree.php
+++ src/muqsit/vanillagenerator/generator/object/tree/BigOakTree.php
@@ -15,14 +15,14 @@ use function array_key_exists;
 use function hypot;
 use function max;
 use function min;
+use function pow;
 use function sqrt;
 
 class BigOakTree extends GenericTree{
-
 	private const LEAF_DENSITY = 1.0;
 
 	private int $maxLeafDistance = 5;
-	private int $trunkHeight;
+	private int $trunkHeight = 0;
 
 	public function __construct(Random $random, BlockTransaction $transaction){
 		parent::__construct($random, $transaction);
@@ -63,12 +63,16 @@ class BigOakTree extends GenericTree{
 		foreach($leafNodes as $node){
 			for($y = 0; $y < $this->maxLeafDistance; ++$y){
 				$size = $y > 0 && $y < $this->maxLeafDistance - 1.0 ? 3.0 : 2.0;
+				$sizeSquared = pow($size, 2);
 				$nodeDistance = (int) (0.618 + $size);
 				for($x = -$nodeDistance; $x <= $nodeDistance; ++$x){
+					$xSquared = pow(abs($x) + 0.5, 2);
 					for($z = -$nodeDistance; $z <= $nodeDistance; ++$z){
-						$sizeX = abs($x) + 0.5;
-						$sizeZ = abs($z) + 0.5;
-						if($sizeX * $sizeX + $sizeZ * $sizeZ <= $size * $size && array_key_exists($world->getBlockAt($node->x + $x, $node->y + $y, $node->z + $z)->getId(), $this->overridables)){
+						$zSquared = pow(abs($z) + 0.5, 2);
+						if(
+							($xSquared + $zSquared <= $sizeSquared) &&
+							array_key_exists($world->getBlockAt($node->x + $x, $node->y + $y, $node->z + $z)->getId(), $this->overridables)
+						){
 							$this->transaction->addBlockAt($node->x + $x, $node->y + $y, $node->z + $z, $this->leavesType);
 						}
 					}
@@ -117,11 +121,16 @@ class BigOakTree extends GenericTree{
 			$dx = (float) $target->x / $maxDistance;
 			$dy = (float) $target->y / $maxDistance;
 			$dz = (float) $target->z / $maxDistance;
-			$height = $world->getMaxY();
+			$maxY = $world->getMaxY();
+			$minY = $world->getMinY();
 			for($i = 0; $i <= $maxDistance; ++$i, ++$n){
 				$target = $from->add(0.5 + $i * $dx, 0.5 + $i * $dy, 0.5 + $i * $dz);
 				$targetFloorY = $target->getFloorY();
-				if($targetFloorY < 0 || $targetFloorY > $height || !array_key_exists($world->getBlockAt($target->getFloorX(), $target->getFloorY(), $target->getFloorZ())->getId(), $this->overridables)){
+				if(
+					$targetFloorY < $minY ||
+					$targetFloorY > $maxY ||
+					!array_key_exists($world->getBlockAt($target->getFloorX(), $target->getFloorY(), $target->getFloorZ())->getId(), $this->overridables)
+				){
 					return $n;
 				}
 			}
@@ -134,33 +143,33 @@ class BigOakTree extends GenericTree{
 	 */
 	private function generateLeafNodes(int $blockX, int $blockY, int $blockZ, ChunkManager $world, Random $random) : array{
 		$leafNodes = [];
-		$y = $blockY + $this->height = $this->maxLeafDistance;
+		$y = $blockY + $this->height - $this->maxLeafDistance;
 		$trunkTopY = $blockY + $this->trunkHeight;
 		$leafNodes[] = new BigOakTreeLeafNode($blockX, $y, $blockZ, $trunkTopY);
 
-		$nodeCount = (int) (1.382 + ((static::LEAF_DENSITY * ($this->height / 13.0)) ** 2.0));
+		$nodeCount = (int) (1.382 + pow((static::LEAF_DENSITY * ($this->height / 13.0)), 2.0));
 		$nodeCount = max($nodeCount, 1);
 
-		for($l = --$y - $blockY; $l >= 0; --$l, --$y){
+		for($l = --$y - $blockY; $l >= 0; --$l, --$y) {
 			$h = $this->height / 2.0;
 			$v = $h - $l;
-			$f = $l < ($this->height * 0.3) ? -1.0 : (
-			$v === $h ? $h * 0.5 : (
-			$h <= abs($v) ? 0.0 : sqrt($h * $h - $v * $v) * 0.5
-			)
-			);
-			if($f >= 0.0){
-				for($i = 0; $i < $nodeCount; ++$i){
+			$f = (($l < ($this->height * 0.3)) ? -1.0 : (
+				$v === $h ? $h * 0.5 : (
+					$h <= abs($v) ? 0.0 : (sqrt($h * $h - $v * $v) * 0.5)
+				)
+			));
+			if($f >= 0.0) {
+				for($i = 0; $i < $nodeCount; ++$i) {
 					$d1 = $f * ($random->nextFloat() + 0.328);
 					$d2 = $random->nextFloat() * M_PI * 2.0;
-					$x = (int) ($d1 * MathHelper::getInstance()->sin($d2) + $blockX + 0.5);
-					$z = (int) ($d1 * MathHelper::getInstance()->cos($d2) + $blockZ + 0.5);
-					if($this->countAvailableBlocks(new Vector3($x, $y, $z), new Vector3($x, $y + $this->maxLeafDistance, $z), $world) === -1){
+					$x = (int)($d1 * MathHelper::getInstance()->sin($d2) + $blockX + 0.5);
+					$z = (int)($d1 * MathHelper::getInstance()->cos($d2) + $blockZ + 0.5);
+					if($this->countAvailableBlocks(new Vector3($x, $y, $z), new Vector3($x, $y + $this->maxLeafDistance, $z), $world) === -1) {
 						$offX = $blockX - $x;
 						$offZ = $blockZ - $z;
 						$distance = 0.381 * hypot($offX, $offZ);
-						$branchBaseY = min($trunkTopY, (int) ($y - $distance));
-						if($this->countAvailableBlocks(new Vector3($x, $branchBaseY, $z), new Vector3($x, $y, $z), $world) === -1){
+						$branchBaseY = min($trunkTopY, (int)($y - $distance));
+						if($this->countAvailableBlocks(new Vector3($x, $branchBaseY, $z), new Vector3($x, $y, $z), $world) === -1) {
 							$leafNodes[] = new BigOakTreeLeafNode($x, $y, $z, $branchBaseY);
 						}
 					}
